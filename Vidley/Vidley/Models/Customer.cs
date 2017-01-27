cca3fcca5030f1423aac3a3dd20c10588f481f96
shevchenkotaras <shevchenkotv91@gmail.com>
2017-01-27 11:48:40 +0200

    037-Labels

--- Vidley/Vidley/Models/Customer.cs
+++ Vidley/Vidley/Models/Customer.cs
@@ -13,6 +13,7 @@ namespace Vidley.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
