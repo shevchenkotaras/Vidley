36955ffec2626b78259ef94c971a0e8304bfd963
shevchenkotaras <shevchenkotv91@gmail.com>
2017-01-28 20:33:58 +0200

    041-Edit Form

--- Vidley/Vidley/Controllers/CustomersController.cs
+++ Vidley/Vidley/Controllers/CustomersController.cs
@@ -32,11 +32,11 @@ namespace Vidley.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         public ActionResult Details(int id)
@@ -58,6 +58,24 @@ namespace Vidley.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
