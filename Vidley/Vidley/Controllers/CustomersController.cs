71e7be68fc2907b7715e34c2fa90a2f77388c01f
shevchenkotaras <shevchenkotv91@gmail.com>
2017-02-02 07:26:36 +0200

    054-Anti-forgery Tokens

--- Vidley/Vidley/Controllers/CustomersController.cs
+++ Vidley/Vidley/Controllers/CustomersController.cs
@@ -54,12 +54,14 @@ namespace Vidley.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             var errors = ModelState
                  .Where(x => x.Value.Errors.Count > 0)
                  .Select(x => new { x.Key, x.Value.Errors })
                  .ToArray();
+
             if (!ModelState.IsValid)
             {
 
