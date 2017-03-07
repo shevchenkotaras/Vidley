cfb51399c54ee8927bf3dbb9d021a4766b706c2e
shevchenkotaras <shevchenkotv91@gmail.com>
2017-03-07 08:09:05 +0200

    Add assert for invalid values to api rental method

--- Vidley/Vidley/Controllers/Api/RentalsController.cs
+++ Vidley/Vidley/Controllers/Api/RentalsController.cs
@@ -21,13 +21,25 @@ namespace Vidley.Controllers.Api
         [HttpPost]
         public IHttpActionResult GetRental(NewRentalDto newRental)
         {
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No movie ids have been given");
+
             int customerId = newRental.CustomerId;
-            Customer customerInDb = _dbContext.Customers.Single(x => x.Id == customerId);
+            Customer customerInDb = _dbContext.Customers.SingleOrDefault(x => x.Id == customerId);
+
+            if (customerInDb == null)
+                return BadRequest("CustomerId is not valid");
             
-            var movies = _dbContext.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            List<Movie> movies = _dbContext.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or more movies are invalid");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental()
